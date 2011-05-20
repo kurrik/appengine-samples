88472b82c0abdef9090b8d7f65e14bac8c5266ca
Arne Roomann-Kurrik <kurrik@gmail.com>
2011-05-20 13:22:50 -0400

    Add a simple go echo server

---
+++ go-echo/echo/echo.go
@@ -0,0 +1,59 @@
+package echo
+
+import (
+  "http"
+  "template"
+  "time"
+)
+
+type PostData struct {
+  Content string // post content
+  Seconds int64  // timestamp
+  Host string    // hostname
+}
+
+func init() {
+  http.HandleFunc("/dest", posthandler)
+  http.HandleFunc("/", formhandler)
+}
+
+func formhandler (w http.ResponseWriter, r *http.Request) {
+  if err := formTemplate.Execute(w, nil); err != nil {
+    http.Error(w, err.String(), http.StatusInternalServerError)
+  }
+}
+
+var formTemplate = template.MustParse(formTemplateHTML, nil)
+const formTemplateHTML = `
+<!DOCTYPE html>
+<html>
+  <body>
+    <form method="POST" action="/dest">
+      <input type="text" name="content" placeholder="Put content here" />
+      <button>Submit</button>
+    </form>
+  </body>
+</html>`
+
+func posthandler (w http.ResponseWriter, r *http.Request) {
+  data := PostData {
+    Content: r.FormValue("content"),
+    Seconds: time.Seconds(),
+    Host: r.Host,
+  }
+  
+  if err := postTemplate.Execute(w, data); err != nil {
+    http.Error(w, err.String(), http.StatusInternalServerError)
+  }
+}
+
+var postTemplate = template.MustParse(postTemplateHTML, nil)
+const postTemplateHTML = `
+<!DOCTYPE html>
+<html>
+  <body>
+    <pre>{Content|html}</pre>
+    <p>{Seconds}</p>
+    <a href="http://{Host}/">Back</a>
+  </body>
+</html>`
